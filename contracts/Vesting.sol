13f934f60c7bdaf484bbcd1e040ee164baa44cd1
DuDeXsUraj <surajgusain2004@gmail.com>
2023-10-11 22:41:08 +0530

    new

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -11,13 +11,14 @@ contract Vesting is Ownable {
     uint256 public duration; // In seconds
     uint256 public n;
     uint256 public totalDeposited;
-    uint256 public claimedAmount;
     uint256 public amount;
 
     event Deposited(address indexed account, uint256 amount);
     event Claimed(address indexed account, uint256 amount);
 
     mapping(address => uint256) public deposits; // Mapping to store deposited amounts for each receiver
+  mapping(address => uint256) public claimedAmount; // Mapping to store claimed amounts for each receiver
+
 
     constructor(
         address _token,
@@ -39,8 +40,8 @@ contract Vesting is Ownable {
         emit Deposited(msg.sender, amount);
     }
 
-    function claimableAmount(address receiverAddress) public view returns (uint256) {
-        require(block.timestamp >= startTimestamp || deposits[receiverAddress] == 0 ,"Vesting has not started");
+    function claimableAmount(address receiverAddress) public view returns(uint256) {
+        require(block.timestamp >= startTimestamp || deposits[receiverAddress] == 0 ,"vesting not started");
         uint256 timeElapsed = block.timestamp - startTimestamp;
         uint256 numPeriods = timeElapsed  / duration;
 
@@ -50,19 +51,15 @@ contract Vesting is Ownable {
 
         uint256 tokensPerPeriod = amount / n;
         uint256 claimable = tokensPerPeriod * (numPeriods + 1);
-        if (claimable > totalDeposited) {
-            claimable = totalDeposited;
-        }
-
         return claimable;
     }
 
     function withdraw() external {
         uint256 amountToClaim = claimableAmount(msg.sender);
         require(amountToClaim > 0, "No tokens to claim");
-
+       
         require(token.transfer(msg.sender, amountToClaim), "Token transfer failed");
-
+             claimedAmount[msg.sender] += amountToClaim;
         emit Claimed(msg.sender, amountToClaim);
     }
 }
---
+++ test/vesting.test.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Vesting Contract", function () {
+  let Token, Vesting;
+  let token, vesting, owner, user;
+
+  const DURATION = 86400; // 1 day in seconds
+  const N = 10;
+  const AMOUNT = 1000; // Initial deposit amount
+
+  beforeEach(async function () {
+    [owner, user] = await ethers.getSigners();
+
+    Token = await ethers.getContractFactory("Token");
+    Vesting = await ethers.getContractFactory("Vesting");
+
+    token = await Token.deploy(AMOUNT);
+    await token.deployed();
+
+    vesting = await Vesting.deploy(token.address, DURATION, N);
+    await vesting.deployed();
+  });
+
+  it("Should allow users to deposit tokens", async function () {
+    await token.connect(user).approve(vesting.address, AMOUNT);
+    await vesting.connect(user).deposit(AMOUNT);
+
+    const userDeposits = await vesting.deposits(user.address);
+    expect(userDeposits).to.equal(AMOUNT);
+  });
+
+  it("Should allow users to claim tokens after vesting period", async function () {
+    // Fast-forward time by 10 days
+    await ethers.provider.send("evm_increaseTime", [DURATION * N]);
+    await ethers.provider.send("evm_mine");
+
+    const initialUserBalance = await token.balanceOf(user.address);
+    await vesting.connect(user).withdraw();
+    const finalUserBalance = await token.balanceOf(user.address);
+
+    const claimedAmount = await vesting.claimedAmount(user.address);
+    expect(finalUserBalance.sub(initialUserBalance)).to.equal(claimedAmount);
+  });
+});
